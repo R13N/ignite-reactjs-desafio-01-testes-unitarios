584071e7b6ea352f7e16a4b39e7c82f2eaca1686
RodrigoNiederauer <rodrigonied@hotmail.com>
2021-08-16 11:17:48 -0300

    passando primeiros testes de integracao

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,44 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+
+import { app } from "../../../../app";
+import createConnection from "../../../../database";
+
+let connection: Connection;
+
+describe("Create user controller", ()=> {
+  
+  beforeAll(async()=>{
+    connection = await createConnection();
+    await connection.runMigrations();
+  })
+  
+  afterAll(async() => {
+    await connection.dropDatabase();
+    await connection.close();
+  })
+
+  it("Should be able to create a new user.", async ()=> {
+
+    const response = await request(app).post("/api/v1/users").send({
+      name: "Mario Graves",
+      email: "guotvof@le.to",
+      password: "123456"
+    })
+
+    expect(response.status).toBe(201);
+  })
+  it("Should not be able to create a user with e-mail already in use.", async () =>{
+    const response = await request(app).post("/api/v1/users").send({
+      name: "Mario Graves",
+      email: "guotvof@le.to",
+      password: "123456"
+    })
+
+    console.log(response.status);
+    console.log(response.body);
+
+    expect(response.status).toBe(400);
+  })
+
+})
